099a93930a0ae9bc2e8fbd7e73b99fdce3c0d804
wgeorge1990 <williameverettgeorge@icloud.com>
2019-09-24 17:08:20 -0400

    restful api that serves articles

---
+++ main.go
@@ -0,0 +1,84 @@
+// main.go
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Article struct {
+	Id      string `json:"Id"`
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+var Articles []Article
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "You are on the home page of our RESTFUL go server")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	for _, article := range Articles {
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	//Unmarshall body of rsponse into new Article struct and then append new Article to it.
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var article Article
+	json.Unmarshal(reqBody, &article)
+	//update articles array to include the new article
+	Articles = append(Articles, article)
+
+	json.NewEncoder(w).Encode(article)
+}
+
+func deleteArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	for index, article := range Articles {
+		if article.Id == id {
+			Articles = append(Articles[:index], Articles[index+1:]...)
+		}
+	}
+
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", returnAllArticles)
+	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
+	myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+	log.Fatal(http.ListenAndServe(":3000", myRouter))
+}
+
+func main() {
+	Articles = []Article{
+		Article{Id: "1", Title: "How to build a server with Go", Desc: "This article will walk you through building a resful api with Go.", Content: "Content will continue to grow but at this moment simply serves as a placeholder."},
+		Article{Id: "2", Title: "Why Go is a better alterantive to Node", Desc: "Article Description", Content: "Article Content"},
+		Article{Id: "3", Title: "How to incorperate authentication in a Go application", Desc: "description", Content: "This is where content will go."},
+	}
+	handleRequests()
+}
